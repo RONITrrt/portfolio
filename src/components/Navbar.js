1b2114d15b8c29059b1936a96e7a6a9b4bbf5e8c
RonitRRT <tripathy.ronit2002@gmail.com>
2025-07-31 11:34:00 +0530

    navbar edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/RONITrrt/RONITrrt"
+                href="https://github.com/RONITrrt/portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
